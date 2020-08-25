ba79e8c81136a1d0f1cb955a2b70195fdf29c4e6
darthpilou <70230562+darthpilou@users.noreply.github.com>
2020-08-26 00:04:05 +0200

    Update cctest.js
    clearer comparisons

--- cctest.js
+++ cctest.js
@@ -300,25 +300,15 @@ SK.update = () => {
         let bcur= '';
         let acur= '';
         if (ratio <0.2){
-            bcur='~~';
-            acur='<<';}
+            bcur='~~~';
+            acur='   ';}
         else {
-            if (ratio <0.4){
-                bcur='<';
-                acur='<<';}
+            if (ratio >0.8){
+                bcur='   ';
+                acur='~~~';}
             else {
-                if (ratio <0.6){
-                    bcur='<';
-                    acur='<';}
-                else {
-                    if (ratio <0.8){
-                        bcur='<<';
-                        acur='<';}
-                    else {
-                        bcur='<<';
-                        acur='~~';
-                    }
-                }
+                bcur=' < ';
+                acur=' < ';
             }
         }
         
