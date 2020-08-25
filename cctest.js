a0e9133509c819fc930b6380f065ab20974a1ea9
darthpilou <70230562+darthpilou@users.noreply.github.com>
2020-08-25 23:54:57 +0200

    Update cctest.js
    syntax one too many }

--- cctest.js
+++ cctest.js
@@ -316,7 +316,7 @@ SK.update = () => {
                         acur='<';}
                     else {
                         bcur='<<';
-                        acur='~~';}
+                        acur='~~';
                     }
                 }
             }
