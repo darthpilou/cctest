67a0c9373e493559fa9c983ea11cbe311a219f7a
darthpilou <70230562+darthpilou@users.noreply.github.com>
2020-08-28 13:54:55 +0200

    Update cctest.js
    still no rgb

--- cctest.js
+++ cctest.js
@@ -276,9 +276,9 @@ cctest.updateProgressBar = (good,id,row) => {
 		color2 = "#1f2836";
 		
 		opac = 1-ratio;
-		let red = 255*ratio;
-		let green = 255*opac;
-		color1 = "rgb(" + red.toFixed(0) + "," + green.toFixed(0)  + ", 0);";
+		let red = (int) 255*ratio;
+		let green = (int) 255*opac;
+		color1 = "rgb(" + red + "," + green  + ", 0)";
 		if(range<30 || opac<0.1)
 			opac=0.1;
 		if(ratio < 0.2 && range>30)
